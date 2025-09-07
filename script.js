e64bb47fa5fab9ab3e67c3ed6744d0bd08d3ddab
JongYoun3128 <whddbs3128@gmail.com>
2025-09-07 17:50:46 +0900

    hyun

---
+++ script.js
@@ -0,0 +1,46 @@
+const idBox = document.querySelector(".id-box");
+const passwordBox = document.querySelector(".password-box");
+
+const member = async () => {
+    let formData = {
+        memberId: idBox.value,
+        password: passwordBox.value,
+        isAdult: true,
+    };
+    const res = await fetch("http://tomhoon.my:12000/api/v1/member/join", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+    const 결과 = await res.json();
+    console.log(결과);
+
+    if (결과.status == "success") {
+        alert("회원가입이 완료되었습니다");
+        // Cookie, Session, Local Storage
+        localStorage.setItem("isLogin", "true");
+        location.href = "index.html";
+    } else {
+        alert("회원가입이 실패하였습니다");
+        localStorage.setItem("isLogin", "false");
+    }
+};
+
+const login = async () => {
+    let formData = {
+        memberId: "test1",
+        password: "1234",
+    };
+
+    const res = await fetch("http://tomhoon.my:12000/api/v1/member/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const 결과 = await res.json();
+};
